107323d029597f762549b12cc02b9fc08260702c
dmarlop <davidmarinlpz@gmail.com>
2024-04-03 12:54:08 +0200

    Commit inicial

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-raiz',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'cursoEmpleados';
+  saludo = "Hola gente";
+}
